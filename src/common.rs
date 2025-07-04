6123fd70fc3423abb469fc21655fccce6f788d00
qingyun <17702702726@163.com>
2025-07-04 23:02:12 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://59.153.164.202:21114".to_owned()
 }
 
 #[inline]
